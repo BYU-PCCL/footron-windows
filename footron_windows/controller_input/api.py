659bc5bce8db171dd5add05caab7662b874d52f4
chris-luangrath <thechrisluangrath@gmail.com>
2022-05-12 17:41:01 -0600

    functioning, need to clean up now

--- footron_windows/controller_input/api.py
+++ footron_windows/controller_input/api.py
@@ -12,20 +12,14 @@ class LastControllerInputApi:
         return f"{self.url}{endpoint}"
 
     def set_last_input(self, last_input):
-        # print("last input updated")
-        # print(last_input)
-        if last_input[0] != self.last_input_time or last_input[1] == True:
-            print(str(last_input[0]) + ", holding = " + str(last_input[1]))
-            self.last_input_time = last_input[0]
+        if last_input != self.last_input_time:
+            print(last_input)
+            self.last_input_time = last_input
             # response = requests.patch(
             #     self._url_with_endpoint(_ENDPOINT_CURRENT_EXPERIENCE),
-            #     json={"last_interaction": last_input[0]},
+            #     json={"last_interaction": last_input},
             # )
         else:
             print("no change")
-
-        # error codes
-        # if response.status_code == x:
-        #     #hmmmmm
 
         return True
--- footron_windows/controller_input/gamepad.py
+++ footron_windows/controller_input/gamepad.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from xinput import get_gamepad
+from XInput import get_events, EVENT_DISCONNECTED
 from threading import Thread
 import datetime as dt
 from time import sleep, time
@@ -19,7 +19,8 @@ CONTROLLER_URL = (
 class LastControllerInput:
     def __init__(self):
         self._api = LastControllerInputApi(CONTROLLER_URL)
-        self.last_interaction = [int(time() * 1000), False]
+        self.last_interaction = time() * 1000
+        # self.last_interaction = [int(time() * 1000), False]
         self.holding = [{} for _ in range(4)]  # 4 controllers
 
         # threads for getting the inputs and posting the latest input
@@ -31,7 +32,7 @@ class LastControllerInput:
         # set_last_input_loop_thread.setDaemon(True)
         get_input_loop_thread.start()
         set_last_input_loop_thread.start()
-        self.gamepads = None
+        # self.gamepads = None
         while True:
             pass
 
@@ -40,53 +41,35 @@ class LastControllerInput:
         while True:
             events = get_events()
             for event in events:
-                last_input_time = datetime.now()
-                # print(event)
-                if event.type == 2:
-                    holding[event.user_index] = {}
-                elif hasattr(event, "button"):
-                    if event.button == "GUIDE":
-                        print("guide")
-                    else:
-                        holding[event.user_index][event.button] = (
-                            True if event.type == 3 else False
-                        )
-                elif hasattr(event, "trigger"):
-                    holding[event.user_index]["trigger" + str(event.trigger)] = (
-                        True if event.value >= 0.001 else False
-                    )
-                elif hasattr(event, "stick"):
-                    holding[event.user_index]["stick" + str(event.stick)] = (
-                        True if event.value >= 0.01 else False
-                    )
+                if event.type != EVENT_DISCONNECTED:
+                    self.last_interaction = time() * 1000
+                # # print(event)
+                # if event.type == 2:
+                #     self.holding[event.user_index] = {}
+                # elif hasattr(event, "button"):
+                #     if event.button == "GUIDE":
+                #         print("guide")
+                #     else:
+                #         self.holding[event.user_index][event.button] = (
+                #             True if event.type == 3 else False
+                #         )
+                # elif hasattr(event, "trigger"):
+                #     self.holding[event.user_index]["trigger" + str(event.trigger)] = (
+                #         True if event.value >= 0.001 else False
+                #     )
                 # elif hasattr(event, "stick"):
-                #     holding[event.user_index]["stick" + str(event.stick)] = (
-                #         True if (abs(event.x) >= 0.025 or abs(event.y) >= 0.025) else False
+                #     self.holding[event.user_index]["stick" + str(event.stick)] = (
+                #         True if event.value >= 0.01 else False
                 #     )
 
-                # print(event)
-                print(is_held())
-                # if hasattr(event, "stick"):
-                #     print(event)
-                #     print(is_held())
-
-    def is_held(self):
-        # what happens with multiple controllers?
-        # for sticks: add 1 when something being held, subtract one when no longer being held
-        # for buttons: add 1 to general total?
-
-        # for everything?? just have a general total and return true when not 0, false when 0
-
-        return True in self.holding.values()
+    # def is_held(self):
+    #     return True in self.holding.values()
 
     # periodically sets the last input through the api
     def update_last_input_loop(self):
         while True:
             sleep(LAST_CONTROLLER_INPUT_SET_DELAY)
             self._api.set_last_input(self.last_interaction)
-
-    def update_gamepads(self):
-        self.gamepads = DeviceManager().gamepads
 
 
 test = LastControllerInput()
