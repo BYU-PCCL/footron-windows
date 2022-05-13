469b677cc88cc9953802e02c2c136ac54ef0c7e0
chris-luangrath <thechrisluangrath@gmail.com>
2022-05-12 18:29:43 -0600

    runs lastControllerInput() on startup

--- footron_windows/app.py
+++ footron_windows/app.py
@@ -5,6 +5,8 @@ from typing import Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+from footron_windows.controller_input.gamepad import LastControllerInput
+
 from .controller import WindowsController
 
 app = FastAPI()
@@ -19,6 +21,15 @@ class CurrentExperience(BaseModel):
     path: Optional[str]
 
 
+last_controller_input = None
+
+
+@app.on_event("startup")
+async def startup_event():
+    global last_controller_input
+    last_controller_input = LastControllerInput()
+
+
 @app.put("/current")
 async def root(body: CurrentExperience):
     if not body.id:
@@ -29,6 +40,8 @@ async def root(body: CurrentExperience):
         experience_path = BASE_EXPERIENCE_PATH / body.id
         binary_path = (experience_path / (re.sub(r"\.\./", "", body.path))).resolve()
         if not binary_path.is_relative_to(BASE_EXPERIENCE_PATH):
-            raise HTTPException(status_code=400, detail=f"Invalid or unauthorized path '{body.path}'")
+            raise HTTPException(
+                status_code=400, detail=f"Invalid or unauthorized path '{body.path}'"
+            )
         await controller.set_current(body.id, binary_path)
     return {"status": "ok"}
