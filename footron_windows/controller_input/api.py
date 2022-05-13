45441b9880f6deafcee2c707679a187e66a6fc08
chris-luangrath <thechrisluangrath@gmail.com>
2022-05-12 18:46:49 -0600

    removed conditional logic, cleaned up

--- footron_windows/controller_input/api.py
+++ footron_windows/controller_input/api.py
@@ -6,20 +6,20 @@ _ENDPOINT_CURRENT_EXPERIENCE = "/current"
 class LastControllerInputApi:
     def __init__(self, url):
         self.url = url
-        self.last_input_time = None
 
     def _url_with_endpoint(self, endpoint) -> str:
         return f"{self.url}{endpoint}"
 
-    def set_last_input(self, last_input):
-        if last_input != self.last_input_time:
-            print(last_input)
-            self.last_input_time = last_input
-            response = requests.patch(
-                self._url_with_endpoint(_ENDPOINT_CURRENT_EXPERIENCE),
-                json={"last_interaction": last_input},
-            )
-        else:
-            print("no change")
 
+    def get_current(self):
+        return requests.get(
+            self._url_with_endpoint(_ENDPOINT_CURRENT_EXPERIENCE)
+        ).json()
+
+
+    def set_last_input(self, last_input):
+        response = requests.patch(
+            self._url_with_endpoint(_ENDPOINT_CURRENT_EXPERIENCE),
+            json={"last_interaction": last_input},
+        )
         return True
